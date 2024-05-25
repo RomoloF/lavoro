a436813b75561c2a02964b8632af137ec05b7663
Romolo1964 <fiorenzaromolo4@gmail.com>
2024-05-25 11:09:05 +0200

    Progetto Funzionante

---
+++ preventivo2Funzionante/src/main/java/it/preventivo/controller/LavoriRestauroController.java
@@ -0,0 +1,68 @@
+package it.preventivo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import it.preventivo.entity.LavoriRestauro;
+import it.preventivo.service.LavoriRestauroService;
+
+@Controller
+@RequestMapping("/lavoriRestauro")
+public class LavoriRestauroController {
+
+    @Autowired
+    private LavoriRestauroService lavoriRestauroService;
+
+//    @GetMapping("/list")
+//    public String list(Model model) {
+//
+//        model.addAttribute("lavoriRestauroList", lavoriRestauroService.findAll());
+//        return "lavoriRestauro/list";
+//    }
+
+
+    @GetMapping
+    public String listLavoriRestauro(Model model) {
+        List<LavoriRestauro> lavoriRestauro = lavoriRestauroService.findAll();
+        model.addAttribute("lavoriRestauro", lavoriRestauro);
+        return "lavoriRestauro/list";
+    }
+    @GetMapping("/create")
+    public String createForm(Model model) {
+        model.addAttribute("lavoriRestauro", new LavoriRestauro());
+        return "lavoriRestauro/form";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute LavoriRestauro lavoriRestauro) {
+        lavoriRestauroService.save(lavoriRestauro);
+        return "redirect:/lavoriRestauro/list";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editForm(@PathVariable Long id, Model model) {
+        model.addAttribute("lavoriRestauro", lavoriRestauroService.findById(id).orElse(null));
+        return "lavoriRestauro/form";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String edit(@ModelAttribute LavoriRestauro lavoriRestauro) {
+        lavoriRestauroService.save(lavoriRestauro);
+        return "redirect:/lavoriRestauro/list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        lavoriRestauroService.deleteById(id);
+        return "redirect:/lavoriRestauro/list";
+    }
+}
+
